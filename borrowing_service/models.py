f9950fe642eb8c1f47c783593bb8a1282d380d56
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-25 12:59:55 +0200

    started app "borrowing_service", created a Borrowing model

---
+++ borrowing_service/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from books.models import Book
+from rest_framework.exceptions import ValidationError
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"Borrowed Book ID: {self.book_id},"
+                f" User ID: {self.user_id}, "
+                f"Borrow Date: {self.borrow_date}, "
+                f"Expected Return Date: {self.expected_return_date}, "
+                f"Actual Return Date: {self.actual_return_date}"
+                )
+
+    def clean(self):
+        if self.expected_return_date < self.borrow_date:
+            raise ValidationError(
+                "Expected return date cannot be earlier than the borrow date."
+            )
+        if (self.actual_return_date and
+                (self.actual_return_date < self.borrow_date or
+                 self.actual_return_date > self.expected_return_date)):
+            raise ValidationError(
+                "Actual return date must be between "
+                "borrow date and expected return date."
+            )
