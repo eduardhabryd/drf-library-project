f970443344488eb92d70bfe5003747572d6cfce5
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-25 13:03:23 +0200

    create serializer for Borrowing model,register Borrowing in admin

---
+++ borrowing_service/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from books.models import Book
+from .models import Borrowing
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    pass
+
+
+class BorrowingDetailSerializer(serializers.ModelSerializer):
+    book_info = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Borrowing
+        fields = '__all__'
+
+    def get_book_info(self, obj):
+        book = object.book_id
+        return BookSerializer(book).data
