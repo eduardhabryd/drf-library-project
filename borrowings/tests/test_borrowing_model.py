fcb0f355cf39f443f526d90995d38869a713c7dd
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-25 19:00:23 +0200

    added test for model

---
+++ borrowings/tests/test_borrowing_model.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from datetime import date
+from borrowings.models import Borrowing
+from books.models import Book
+from users.models import User
+
+
+class BorrowingModelTests(TestCase):
+    def test_create_borrowing(self):
+        book = Book.objects.create(
+            title="Test Book", author="Test Author", inventory=10, daily_fee=10.00
+        )
+
+        user = User.objects.create(email="test@example.com", password="testpassword")
+
+        borrowing = Borrowing(
+            borrow_date=date(2023, 11, 10),
+            expected_return_date=date(2023, 11, 12),
+            actual_return_date=date(2023, 11, 11),
+            book_id=book,
+            user_id=user,
+        )
+        borrowing.save()
+
+        saved_borrowing = Borrowing.objects.get(pk=borrowing.pk)
+
+        self.assertEqual(saved_borrowing.borrow_date, date(2023, 11, 10))
+        self.assertEqual(saved_borrowing.expected_return_date, date(2023, 11, 12))
+        self.assertEqual(saved_borrowing.actual_return_date, date(2023, 11, 11))
+        self.assertEqual(saved_borrowing.book_id, book)
+        self.assertEqual(saved_borrowing.user_id, user)
