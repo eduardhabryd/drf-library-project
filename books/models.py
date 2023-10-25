4d3b322513461fab851e7e3133c6c1a87c1baa98
Taras Borshch <tarasborshch4@gmail.com>
2023-10-25 13:10:02 +0300

    Create book model and register book app in settings

--- books/models.py
+++ books/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+
+class Book(models.Model):
+    class Covers(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=127)
+    author = models.CharField(max_length=127)
+    cover = models.CharField(
+        max_length=4,
+        choices=Covers.choices,
+        default=Covers.HARD
+    )
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField()
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.inventory})"
