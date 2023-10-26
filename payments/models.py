869ed1ce2ba12d81b058b828be951bb0c002ca0a
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-27 02:11:41 +0300

    add related name to borrowing field

--- payments/models.py
+++ payments/models.py
@@ -9,7 +9,7 @@ class Payment(models.Model):
 
     status_payment = models.CharField(max_length=7, choices=STATUS_CHOICES)
     type_payment = models.CharField(max_length=7, choices=TYPE_CHOICES)
-    borrowing = models.OneToOneField(Borrowing, on_delete=models.CASCADE)
+    borrowing = models.OneToOneField(Borrowing, on_delete=models.CASCADE, related_name="payment")
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
     money = models.DecimalField(max_digits=5, decimal_places=2)
