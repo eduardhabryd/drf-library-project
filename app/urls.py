dff8b36327d4eeea4b789197924efee453062de1
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-10-26 16:22:34 +0200

    add swagger documentation

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/borrowings/", include(
+        "borrowings.urls",
+        namespace="borrowings"
+    )),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/borrowings/",
-        include("borrowings.urls", namespace="borrowings")
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
 ]
