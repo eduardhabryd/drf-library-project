c982e9e53e8cfc3f925f10ffabe3945f59947a5f
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-27 01:22:06 +0300

    override get_context_data in PayView to pass borrowing_id to context; add book data to stripe.session

--- stripe_pay/views.py
+++ stripe_pay/views.py
@@ -4,9 +4,18 @@ from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
 
+from borrowings.models import Borrowing
+from books.models import Book
+
 
 class PayView(TemplateView):
     template_name = "pay.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        borrowing_id = self.kwargs.get('borrowing_id')
+        context['borrowing_id'] = borrowing_id
+        return context
 
 
 @csrf_exempt
@@ -17,10 +26,14 @@ def stripe_config(request):
 
 
 @csrf_exempt
-def create_checkout_session(request):
+def create_checkout_session(request, borrowing_id=None):
     if request.method == "GET":
         domain_url = "http://localhost:8000/"
         stripe.api_key = settings.STRIPE_SECRET_KEY
+        borrowing = Borrowing.objects.get(pk=borrowing_id)
+        book = Book.objects.get(pk=borrowing.book.id)
+        book_name = book.title
+        price = int(book.daily_fee * 100)
         try:
             # Create new Checkout Session for the order
             # Other optional params include:
@@ -39,11 +52,18 @@ def create_checkout_session(request):
                 mode="payment",
                 line_items=[
                     {
-                        "price": "price_1O5TGDCwPQ6rrxhRtGC7FQUS",
-                        "quantity": 1,
+                        'price_data': {
+                            'currency': 'usd',
+                            'product_data': {
+                                'name': book_name,
+                            },
+                            'unit_amount': price,
+                        },
+                        'quantity': 1,
                     }
                 ],
             )
+            
             return JsonResponse({"sessionId": checkout_session["id"]})
         except Exception as e:
             return JsonResponse({"error": str(e)})
