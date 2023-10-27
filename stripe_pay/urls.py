2e98adf5e224c5a1443d4f2b1a357e18aada0f44
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-27 14:48:17 +0300

    add path for pay-fine and create-checkout-session-fine

--- stripe_pay/urls.py
+++ stripe_pay/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
-from stripe_pay.views import PayView, stripe_config, create_checkout_session, success_view, cancelled_view
+from stripe_pay.views import PayView, stripe_config, create_checkout_session, success_view, cancelled_view, PayFineView, create_checkout_session_fine
 
 urlpatterns = [
     path("pay/<int:borrowing_id>/", PayView.as_view(), name="pay"),
+    path("pay-fine/<int:borrowing_id>/", PayFineView.as_view(), name="pay-fine"),
     path("config/", stripe_config),
     path(
         "create-checkout-session/<int:borrowing_id>/",
         create_checkout_session,
         name="create-checkout-session"
+    ),
+    path(
+        "create-checkout-session-fine/<int:borrowing_id>/",
+        create_checkout_session_fine,
+        name="create-checkout-session-fine"
     ),
     path("success/", success_view, name="success"),
     path("cancel/", cancelled_view, name="cancel"),
