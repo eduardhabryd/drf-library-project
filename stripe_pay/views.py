883567a98105f0f40c991dab13f3aff20df73656
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-10-27 14:48:51 +0300

    create PayFineView and create_checkout_session_fine

--- stripe_pay/views.py
+++ stripe_pay/views.py
@@ -1,6 +1,7 @@
 import stripe
 from django.conf import settings
 from django.http.response import JsonResponse
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
 from rest_framework.decorators import api_view
@@ -19,6 +20,16 @@ class PayView(TemplateView):
         borrowing_id = self.kwargs.get("borrowing_id")
         context["borrowing_id"] = borrowing_id
         return context
+    
+
+class PayFineView(TemplateView):
+    template_name = "pay_fine.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        borrowing_id = self.kwargs.get("borrowing_id")
+        context["borrowing_id"] = borrowing_id
+        return context
 
 
 @csrf_exempt
@@ -31,7 +42,7 @@ def stripe_config(request):
 @csrf_exempt
 def create_checkout_session(request, borrowing_id=None):
     if request.method == "GET":
-        domain_url = "http://localhost:8000/"
+        domain_url = 'http://localhost:8000/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         borrowing = Borrowing.objects.get(pk=borrowing_id)
         book = Book.objects.get(pk=borrowing.book.id)
@@ -41,9 +52,8 @@ def create_checkout_session(request, borrowing_id=None):
         price = int(book.daily_fee * 100 * days)
         try:
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url
-                + "api/stripe_pay/success?session_id={CHECKOUT_SESSION_ID}",
-                cancel_url=domain_url + "api/stripe_pay/cancel/",
+                success_url=domain_url + 'api/stripe_pay/success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'api/stripe_pay/cancel/',
                 payment_method_types=["card"],
                 mode="payment",
                 line_items=[
@@ -62,6 +72,49 @@ def create_checkout_session(request, borrowing_id=None):
             Payment.objects.create(
                 status_payment="PEN",
                 type_payment="PAY",
+                borrowing=borrowing,
+                session_url=checkout_session.url,
+                session_id=checkout_session.id,
+                money=price / 100,
+            )
+            return JsonResponse({"sessionId": checkout_session["id"]})
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+FINE_MULTIPLIER = 2
+
+def create_checkout_session_fine(request, borrowing_id=None):
+    if request.method == "GET":
+        domain_url = 'http://localhost:8000/'
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        borrowing = Borrowing.objects.get(pk=borrowing_id)
+        book = Book.objects.get(pk=borrowing.book.id)
+        book_name = book.title
+        days = borrowing.actual_return_date - borrowing.expected_return_date
+        days = days.days
+        price = int(book.daily_fee * 100 * days * FINE_MULTIPLIER)
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                success_url=domain_url + 'api/stripe_pay/success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'api/stripe_pay/cancel/',
+                payment_method_types=["card"],
+                mode="payment",
+                line_items=[
+                    {
+                        "price_data": {
+                            "currency": "usd",
+                            "product_data": {
+                                "name": f"Pay fine for {book_name}",
+                            },
+                            "unit_amount": price,
+                        },
+                        "quantity": 1,
+                    }
+                ],
+            )
+            Payment.objects.create(
+                status_payment="PEN",
+                type_payment="FIN",
                 borrowing=borrowing,
                 session_url=checkout_session.url,
                 session_id=checkout_session.id,
