3ed031b41ed7defd91204fbf1986de9f4247bcec
Oleh Chernous <chernous.oleh@gmail.com>
2023-11-07 13:34:41 +0100

    Modify view.py in stripe_pay - modify create_checkout_session and create_checkout_session_fine

--- stripe_pay/views.py
+++ stripe_pay/views.py
@@ -12,6 +12,7 @@ from app.bot_service.bot import successful_notification_handler
 from borrowings.models import Borrowing
 from books.models import Book
 from payments.models import Payment
+from .stripe_services import create_checkout_session, create_checkout_sesison_fine
 
 
 class PayView(TemplateView):
@@ -45,42 +46,9 @@ def stripe_config(request):
 def create_checkout_session(request, borrowing_id=None):
     if request.method == "GET":
         domain_url = "http://localhost:8000/"
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        borrowing = Borrowing.objects.get(pk=borrowing_id)
-        book = Book.objects.get(pk=borrowing.book.id)
-        book_name = book.title
-        days = borrowing.expected_return_date - borrowing.borrow_date
-        days = days.days
-        price = int(book.daily_fee * 100 * days)
         try:
-            checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url
-                + "api/stripe_pay/success?session_id={CHECKOUT_SESSION_ID}",
-                cancel_url=domain_url + "api/stripe_pay/cancel/",
-                payment_method_types=["card"],
-                mode="payment",
-                line_items=[
-                    {
-                        "price_data": {
-                            "currency": "usd",
-                            "product_data": {
-                                "name": book_name,
-                            },
-                            "unit_amount": price,
-                        },
-                        "quantity": 1,
-                    }
-                ],
-            )
-            Payment.objects.create(
-                status_payment="PEN",
-                type_payment="PAY",
-                borrowing=borrowing,
-                session_url=checkout_session.url,
-                session_id=checkout_session.id,
-                money=price / 100,
-            )
-            return JsonResponse({"sessionId": checkout_session["id"]})
+            session_id = create_checkout_session(domain_url, borrowing_id)
+            return JsonResponse({"sessionId": session_id})
         except Exception as e:
             return JsonResponse({"error": str(e)})
 
@@ -92,42 +60,9 @@ FINE_MULTIPLIER = 2
 def create_checkout_session_fine(request, borrowing_id=None):
     if request.method == "GET":
         domain_url = "http://localhost:8000/"
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        borrowing = Borrowing.objects.get(pk=borrowing_id)
-        book = Book.objects.get(pk=borrowing.book.id)
-        book_name = book.title
-        days = borrowing.actual_return_date - borrowing.expected_return_date
-        days = days.days
-        price = int(book.daily_fee * 100 * days * FINE_MULTIPLIER)
         try:
-            checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url
-                + "api/stripe_pay/success?session_id={CHECKOUT_SESSION_ID}",
-                cancel_url=domain_url + "api/stripe_pay/cancel/",
-                payment_method_types=["card"],
-                mode="payment",
-                line_items=[
-                    {
-                        "price_data": {
-                            "currency": "usd",
-                            "product_data": {
-                                "name": f"Pay fine for {book_name}",
-                            },
-                            "unit_amount": price,
-                        },
-                        "quantity": 1,
-                    }
-                ],
-            )
-            Payment.objects.create(
-                status_payment="PEN",
-                type_payment="FIN",
-                borrowing=borrowing,
-                session_url=checkout_session.url,
-                session_id=checkout_session.id,
-                money=price / 100,
-            )
-            return JsonResponse({"sessionId": checkout_session["id"]})
+            session_id = create_checkout_sesison_fine(domain_url, borrowing_id, FINE_MULTIPLIER)
+            return JsonResponse({"sessionId": session_id})
         except Exception as e:
             return JsonResponse({"error": str(e)})
 
